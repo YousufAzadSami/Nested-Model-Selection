83a295b195c08fe9a1b54a905f42b0857e92cf1f
Yousuf Azad (Sami) <sami1592@gmail.com>
2018-12-25 22:28:33 +0100

    adds a script for the buttions, when tranlation or rotation button will be preseed

---
+++ Assets/Scripts/TransformationSelection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformationSelection : MonoBehaviour {
+
+	private GameObject selectedObject;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void setSelectedObject(GameObject inSelectedObject)
+	{
+		selectedObject = inSelectedObject;
+	}
+}
