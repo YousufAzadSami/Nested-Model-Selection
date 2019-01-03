7d00e82ac6252414b61b499caa4f2973e9ac52ed
Yousuf Azad (Sami) <sami1592@gmail.com>
2019-01-03 11:47:02 +0100

    renames a variable

--- Assets/Scripts/TransformationSelection.cs
+++ Assets/Scripts/TransformationSelection.cs
@@ -11,13 +11,13 @@ public class TransformationSelection : MonoBehaviour {
 		Scale
 	}
 
-	public MouseClickDetection mouseClickSelection;
+	public MouseClickDetection mouseClickDetection;
 
 	void Awake () {
 		// not the best of soulution, but will go with the adhoc approach for now
-		mouseClickSelection = FindObjectOfType<MouseClickDetection>();
+		mouseClickDetection = FindObjectOfType<MouseClickDetection>();
 
-		if(mouseClickSelection == null)
+		if(mouseClickDetection == null)
 		{
 			Debug.LogError("mouseClickSelection NOT FOUND!!!");
 		}
@@ -42,12 +42,12 @@ public class TransformationSelection : MonoBehaviour {
 		// The button/s should be visible only when an object is selected
 		// And if an object is selelcted, then this condition should be true
 		// In other words, this condition should always be true
-		if(mouseClickSelection.GetSelectedGameObject())
+		if(mouseClickDetection.GetSelectedGameObject())
 		{
 			// all selectable GameObjects should have the same transformation mode. 
 			// If the user selects the translate button for one object then, 
 			// selects another object user would expect it to tranlate as well
-			TransformationAndHighlight[] _allSelectableGameObjects = mouseClickSelection.FindAllSelectableGameObjects();
+			TransformationAndHighlight[] _allSelectableGameObjects = mouseClickDetection.FindAllSelectableGameObjects();
 			foreach(TransformationAndHighlight _selectableGameObject in _allSelectableGameObjects)
 			{
 				_selectableGameObject.ChangeModes(_translate, _rotate, _scale);
@@ -55,7 +55,7 @@ public class TransformationSelection : MonoBehaviour {
 		}
 		else 
 		{
-			Debug.LogError("Something is wrong, check comment", mouseClickSelection.GetSelectedGameObject());
+			Debug.LogError("Something is wrong, check comment", mouseClickDetection.GetSelectedGameObject());
 		}
 	}
 
