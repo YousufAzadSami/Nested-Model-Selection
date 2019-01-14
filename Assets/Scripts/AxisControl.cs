7039c06498ff9f8e056ea8399c12edda39afcdd2
Yousuf Azad (Sami) <yousuf.azad@iem.fraunhofer.de>
2019-01-14 17:20:35 +0100

    increasing x value from UI

--- Assets/Scripts/AxisControl.cs
+++ Assets/Scripts/AxisControl.cs
@@ -4,23 +4,39 @@ using UnityEngine;
 
 public class AxisControl : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    private bool isXDown;
+    private float xValueSign;
+    private float xValue;
+    private float maxXValue;
+
+    // Use this for initialization
+    void Start () {
+        isXDown = false;
+        xValueSign = 0;
+        xValue = 0;
+        maxXValue = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (isXDown)
+        {
+            // call the functions necessary to tranlate
+            xValue = Mathf.MoveTowards(xValue, maxXValue * xValueSign, Time.deltaTime);
+        }
 	}
 
-	public void XDown() 
+	public void XDown(float inXValue) 
 	{
+        isXDown = true;
+        xValueSign = inXValue;
+        xValue = 0;
+    }
 
-	}
-
-	public void XUp()
+	public void XUp(float inXValue)
 	{
-		
-	}
+        isXDown = false;
+        xValueSign = inXValue;
+        xValue = 0;
+    }
 }
