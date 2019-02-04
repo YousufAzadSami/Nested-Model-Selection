74f0655cf61d20443571ed378ea201b65382c1d4
Yousuf Azad (Sami) <yousuf.azad@iem.fraunhofer.de>
2019-02-04 15:41:04 +0100

    adds a test scene and script to play around with CrossPlatformInput manager

---
+++ Assets/Scripts/InputTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class InputTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Debug.Log("Button : Jump, Button name : \"Jump\"");
+        }
+
+        if (CrossPlatformInputManager.GetButtonDown("CustomJump"))
+        {
+            Debug.Log("Button : Jump, Button name : \"CustomJump\"");
+        }
+
+        if (CrossPlatformInputManager.GetAxis("Vertical") > .01f)
+        {
+            Debug.Log("Slidebar : " + CrossPlatformInputManager.GetAxis("Vertical"));
+        }
+
+        Debug.Log("For the MobileAircraftControls>Right prefab : " + CrossPlatformInputManager.GetAxis("Horizontal"));
+    }
+}
